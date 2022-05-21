262101efd5e474750f4dc9289ffef41ee9fb4321
Nikolay Krishtopa <nickey87@yandex.ru>
2022-05-22 01:13:23 +0300

    bugfix/fix review markups

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,25 @@
 let editProfileButton = document.querySelector('.profile__edit-button')
 let popup = document.querySelector('.popup')
 let popupCloseButton = popup.querySelector('.popup__close-button')
-let popupSubmitButton = popup.querySelector('.popup__submit-button')
 let userNameField = popup.querySelector('.popup__field_type_user-name')
 let userDescrField = popup.querySelector('.popup__field_type_user-description')
 
+const showContent = (obj, cls) => obj.classList.add(cls)
+const hideContent = (obj, cls) => obj.classList.remove(cls)
+
 editProfileButton.addEventListener('click', ()=>{
-  popup.classList.add('popup_active')
+  showContent(popup, 'popup_active')
   userNameField.value = document.querySelector('.profile__name').textContent
   userDescrField.value = document.querySelector('.profile__description').textContent
 }
 )
 
-popupCloseButton.addEventListener ('click', () => {
-  popup.classList.remove('popup_active')
-}
-)
+popupCloseButton.addEventListener ('click', () => hideContent(popup, 'popup_active'))
 
-popupSubmitButton.addEventListener('click', (evt) => {
+popup.querySelector('.popup__edit-profile-form').addEventListener('submit', (evt) => {
   document.querySelector('.profile__name').textContent = userNameField.value
   document.querySelector('.profile__description').textContent = userDescrField.value
-  popup.classList.remove('popup_active')
+  hideContent(popup, 'popup_active')
   evt.preventDefault()
 }
 )
